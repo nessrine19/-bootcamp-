abd25d7054e5d6bf342a5a3e6c27f583cfed1ae5
nessrine19 <89046879+nessrine19@users.noreply.github.com>
2021-12-23 10:58:23 +0100

    Update ex1.py

--- unit_1/homeworks/Functions Homework/ex1.py
+++ unit_1/homeworks/Functions Homework/ex1.py
@@ -1,1 +1,21 @@
-
+def palindrome():
+    word=input("enter a word :")
+    #words having spaces
+    if (" ") in word:
+        #remove the space 
+        new=word.replace(" ", "")
+        backwards=new[::-1]
+        #check if the word is palindrome
+        if new==backwards:
+            print ("this word is a palindrome")
+        else:
+            print("it's not ")
+    #for words without spaces 
+    else :
+        backwards_1=word[::-1]
+        #check if the word is palindrome
+        if word==backwards_1:
+            print ("this word is a palindrome")
+        else:
+            print(word,"is not a palindrome")
+palindrome()
