dc2e4f692c1271e583cc9bfc0f06aa5edd299731
nessrine19 <89046879+nessrine19@users.noreply.github.com>
2021-12-23 11:18:09 +0100

    Update ex2.py

--- unit_1/homeworks/Functions Homework/ex2.py
+++ unit_1/homeworks/Functions Homework/ex2.py
@@ -1,1 +1,10 @@
-
+def prime():
+    num= int (input ("add a number :"))
+    for i in range (2,num):
+        # If num is divisible by any number between 2 and num
+        if (num%i)==0:
+            print(num,"is not prime")
+        break
+    else:
+        print(num,"is prime")
+prime()
