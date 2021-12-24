1288643e35ac6ac2dddee568f58e90725c27de8d
nessrine19 <89046879+nessrine19@users.noreply.github.com>
2021-12-24 11:55:27 +0100

    Update ex2.py

--- unit_1/homeworks/OOP Homework/ex2.py
+++ unit_1/homeworks/OOP Homework/ex2.py
@@ -1,1 +1,7 @@
+class vehicle :
+    def __init__(self,max_speed,mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
 
+my_car=vehicle(180,2000)
+print(my_car.max_speed,my_car.mileage)
