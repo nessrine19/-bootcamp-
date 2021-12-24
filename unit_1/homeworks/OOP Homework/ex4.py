ffe210c76608d75b39a17a0b59644a0e9ee5a300
nessrine19 <89046879+nessrine19@users.noreply.github.com>
2021-12-24 13:16:25 +0100

    Update ex4.py

--- unit_1/homeworks/OOP Homework/ex4.py
+++ unit_1/homeworks/OOP Homework/ex4.py
@@ -1,1 +1,13 @@
+class vehicle :
+    def __init__(self,name,max_speed,mileage):
+        self.name=name
+        self.max_speed=max_speed
+        self.mileage=mileage
+    
+    def vehicle_method(self):
+        print("bus name :",self.name,"\n","speed :",self.max_speed,"\n","mileage :",self.mileage)
 
+class bus(vehicle):
+    pass
+my_bus=bus("meymey",180,2000)
+my_bus.vehicle_method()
