87c7e50d3e80acd64a05cafbb5a9cb8d827c802b
nessrine19 <89046879+nessrine19@users.noreply.github.com>
2021-12-27 15:59:27 +0100

    Update generate26text.py

--- unit_1/homeworks/input_output/generate26text.py
+++ unit_1/homeworks/input_output/generate26text.py
@@ -1,6 +1,4 @@
-import string, os
-if not os.path.exists("letters"):
-   os.makedirs("letters")
+import string
 for letter in string.ascii_uppercase:
    with open(letter + ".txt", "w") as f:
        f.writelines(letter)
