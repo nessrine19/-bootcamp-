e1d79cf90caa4bf0209816cafe5cd026c5a1ce0e
nessrine19 <89046879+nessrine19@users.noreply.github.com>
2021-12-30 23:37:21 +0100

    Update image_homework.py

--- unit_4/linear_algebra/image_homework.py
+++ unit_4/linear_algebra/image_homework.py
@@ -1,1 +1,12 @@
-
+from PIL import Image
+from numpy import asarray
+#open an image and convert it to array 
+image=Image.open("my_page.jpg")
+to_array=asarray(image)
+print(to_array)
+print(to_array.shape)
+#print the matrix type 
+print(to_array.dtype)
+#transpose 
+transpose=to_array.T
+print(transpose)
